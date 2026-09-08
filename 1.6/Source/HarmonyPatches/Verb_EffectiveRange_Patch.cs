4a065bd0b862274fc1927541b1a92851b799a41f
RubyRed404 <106030364+RubyRed404@users.noreply.github.com>
2026-09-08 08:34:01 +0000

    Update Verb_EffectiveRange_Patch.cs

--- 1.6/Source/HarmonyPatches/Verb_EffectiveRange_Patch.cs
+++ 1.6/Source/HarmonyPatches/Verb_EffectiveRange_Patch.cs
@@ -14,7 +14,7 @@ namespace GojisMiscGenes
             if (__instance is Verb_CastAbility castAbility && castAbility.ability is Psycast)
             {
                 var pawn = castAbility.caster as Pawn;
-                if (pawn != null && pawn.HasActiveGene(DefsOf.Goji_Clairvoyance))
+                if (pawn?.HasActiveGene(DefsOf.Goji_Clairvoyance) is true)
                 {
                     var psySensitivity = pawn.GetStatValue(StatDefOf.PsychicSensitivity);
                     var bonusRange = (int)Math.Floor(psySensitivity - 1f);
